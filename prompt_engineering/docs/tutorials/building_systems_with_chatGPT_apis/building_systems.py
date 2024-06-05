a62870e9bab8d857375eea6bb693ccb8f5978115
AmitKumarJnv <jnvamit1666@gmail.com>
2024-06-05 23:55:16 +0530

    starting with second tutorial for prompting

---
+++ prompt_engineering/docs/tutorials/building_systems_with_chatGPT_apis/building_systems.py
@@ -0,0 +1,26 @@
+import openai 
+
+# load the environment variable, create a .env file and store your OPENAI_API_KEY in that file
+from dotenv import load_dotenv
+load_dotenv()
+
+# for openai version > 1
+client = openai.OpenAI()
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo",temperature=0):
+    messages = messages
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature
+    )
+    return response.choices[0].message.content
