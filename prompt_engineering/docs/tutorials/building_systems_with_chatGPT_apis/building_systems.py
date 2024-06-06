1381f56508523aabc1202e7cc0383272b1d0eee5
AmitKumarJnv <jnvamit1666@gmail.com>
2024-06-06 23:50:18 +0530

    adding simple code

--- prompt_engineering/docs/tutorials/building_systems_with_chatGPT_apis/building_systems.py
+++ prompt_engineering/docs/tutorials/building_systems_with_chatGPT_apis/building_systems.py
@@ -24,3 +24,8 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo",temperature=0):
         temperature=temperature
     )
     return response.choices[0].message.content
+
+if __name__ == "__main__":
+
+    response = get_completion("What is the capital of France?")
+    print(response)
